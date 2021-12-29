9335873e5e0c1f7136930c35ca19748d277b93e9
warugaki <warugaki.web.developer@gmail.com>
2021-12-29 20:06:24 +0900

    storybookのディレクトリはプロジェクトルート直下で管理

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx|svelte)"
   ],
   "addons": [
     "@storybook/addon-links",
--- stories/App.stories.svelte
+++ stories/App.stories.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
-  import App from '../App.svelte';
+  import App from '../src/App.svelte';
 </script>
 
 <Meta title="App" component={App}/>
