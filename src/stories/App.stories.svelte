393583f2a7506966a383b6dc453443612ed13a2b
warugaki <warugaki.web.developer@gmail.com>
2021-12-29 19:32:10 +0900

    Appコンポーネントのstorybookの作成とeslintのルールを追加

---
+++ src/stories/App.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import App from '../App.svelte';
+</script>
+
+<Meta title="App" component={App}/>
+
+<Template let:args>
+  <App {...args}/>
+</Template>
+
+<Story name="Default"/>
